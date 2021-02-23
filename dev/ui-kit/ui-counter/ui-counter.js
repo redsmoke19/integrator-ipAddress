88e811caf513e0fef0965c360d7bf1ce0f8e8b98
Oleg Tsyganov <TsyganovDev@gmail.com>
2021-02-23 22:57:12 +0400

    Сверстал шапку и блок hero и адаптировал под планшеты

--- dev/ui-kit/ui-counter/ui-counter.js
+++ dev/ui-kit/ui-counter/ui-counter.js
@@ -1,25 +1,25 @@
-let counter = function () {
-    let btns = document.querySelectorAll('.counter__btn');
-    btns.forEach(function (item) {
-        item.addEventListener('click', counterState)
-    });
+const counter = function () {
+  const btns = document.querySelectorAll('.counter__btn');
+  btns.forEach(function (item) {
+    item.addEventListener('click', counterState);
+  });
 
-    function counterState() {
-        let dir = this.dataset.direction,
-            inputEl = this.parentElement.previousElementSibling,
-            currentValue = inputEl.value;
-        dir === 'plus' ? counterPlus(inputEl, currentValue) : counterMinus(inputEl, currentValue);
-    }
+  function counterState() {
+    const dir = this.dataset.direction;
+    const inputEl = this.parentElement.previousElementSibling;
+    const currentValue = inputEl.value;
+    dir === 'plus'
+      ? counterPlus(inputEl, currentValue)
+      : counterMinus(inputEl, currentValue);
+  }
 
-    const counterPlus = (el, val) => {
-        el.value = +val + 1;
-    };
+  const counterPlus = (el, val) => {
+    el.value = +val + 1;
+  };
 
-    const counterMinus = (el, val) => {
-        if (val - 1) el.value = +val - 1;
-    };
-
+  const counterMinus = (el, val) => {
+    if (val - 1) el.value = +val - 1;
+  };
 };
-
 
 counter();
